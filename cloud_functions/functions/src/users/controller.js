b231168380039d1d9bd3d7257960a74be95be462
Harsh Bhatt <harshb669@gmail.com>
2021-06-16 10:37:23 +0530

    storing user details in rds

--- cloud_functions/functions/src/users/controller.js
+++ cloud_functions/functions/src/users/controller.js
@@ -1,21 +1,42 @@
 const admin = require("firebase-admin");
+const pool = require("../../config/database");
 
 exports.create = async (req, res) => {
   try {
-    const { displayName, password, email, role } = req.body;
+    const { displayName, password, email, phoneNumber, role } = req.body;
 
     if (!displayName || !password || !email || !role) {
       return res.status(400).json({ message: "Missing fields" });
     }
 
-    const { uid } = await admin.auth().createUser({
+    const { uid, ...user } = await admin.auth().createUser({
       displayName,
       password,
       email,
+      phoneNumber,
     });
+
     await admin.auth().setCustomUserClaims(uid, { role });
 
-    return res.status(201).json({ uid });
+    pool.query(
+      `insert into users(uid, email, display_name, phone_number, photo_url, provider_id, created_at) values(?,?,?,?,?,?,?)`,
+      [
+        uid,
+        user.email,
+        user.displayName,
+        user.phoneNumber,
+        user.photoURL,
+        user.providerData[0].providerId,
+        user.metadata.creationTime,
+      ],
+      (err, results, fields) => {
+        if (err) {
+          return handleError(res, err);
+        }
+        console.log("R: ", results);
+        return res.status(201).json({ uid });
+      }
+    );
   } catch (err) {
     return handleError(res, err);
   }
