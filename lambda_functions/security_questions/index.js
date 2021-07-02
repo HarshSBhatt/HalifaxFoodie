678f77cfac3798975258b2aa01a9297e8e98b9a8
Harsh Bhatt <harshb669@gmail.com>
2021-07-02 14:31:37 +0530

    restaurant signup done backend

--- lambda_functions/security_questions/index.js
+++ lambda_functions/security_questions/index.js
@@ -17,17 +17,17 @@ exports.handler = (event, context, callback) => {
     if (err) {
       callback(err);
     }
-    connection.query(
-      `select uq.question_id, q.question, uq.answer from user_questions uq inner join questions q on uq.question_id=q.question_id where uq.user_id = ?`,
-      [event.uid],
-      (err, results, fields) => {
-        connection.release();
-        if (err) {
-          callback(err);
-        }
-        callback(null, results[0]);
-        process.exit();
+    const query =
+      event.role === "user"
+        ? `select uq.question_id, q.question, uq.answer from user_questions uq inner join questions q on uq.question_id=q.question_id where uq.user_id = ?`
+        : `select rq.question_id, q.question, rq.answer from restaurant_questions rq inner join questions q on rq.question_id=q.question_id where rq.restaurant_id = ?`;
+    connection.query(query, [event.uid], (err, results, fields) => {
+      connection.release();
+      if (err) {
+        callback(err);
       }
-    );
+      callback(null, results[0]);
+      process.exit();
+    });
   });
 };
